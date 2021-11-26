f9c321aa289ea1ef62308ae624d54f5abb06d654
kwongz <kyle.wong917@gmail.com>
2021-11-25 19:03:49 -0500

    responsive display info card. hover styling

--- src/components/DisplayItemInfo.js
+++ src/components/DisplayItemInfo.js
@@ -12,6 +12,26 @@ const handleColorChoice = (color) => {
         <>
             <div className="imageContainer">
                 <img src={info.api_featured_image} alt={info.name} />
+                {/* <ul className='colorHolder'>
+                    {
+                        info.product_colors.map((color, index) => {
+                            return <li key={`${index}${color}`} onClick={()=>handleColorChoice(color)}>
+                                <figure className='colorSphere' style={{backgroundColor:color.hex_value}}></figure>
+                            </li>
+                        })
+                    }
+                </ul>
+                <p>Selected Color: {colorChoice.colour_name}</p> */}
+            </div>
+            <div className="textContent">
+                <p className="brand">{info.brand}</p>
+                <h2>{info.name}</h2>
+                <p className="price">
+                    ${info.price}
+                </p>
+                <p className='description'>{info.description}
+                    <a className='websiteLink'href={info.website_link}>Link to Website</a>
+                </p>
                 <ul className='colorHolder'>
                     {
                         info.product_colors.map((color, index) => {
@@ -22,14 +42,6 @@ const handleColorChoice = (color) => {
                     }
                 </ul>
                 <p>Selected Color: {colorChoice.colour_name}</p>
-            </div>
-            <div className="textContent">
-                <p className="brand">{info.brand}</p>
-                <h2>{info.name}</h2>
-                <p className="price">
-                    ${info.price}
-                </p>
-                <p className='description'>{info.description}<a className='websiteLink'href={info.website_link}>Link to Website</a></p>
             <button onClick={() => handleOnClick(colorChoice)}>Add to Cart</button>
             </div>
         </>
